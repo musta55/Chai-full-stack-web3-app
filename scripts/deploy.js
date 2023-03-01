1fcd92d6f03a8322a28f36205fff88f7803ff985
musta55 <bsse1114@iit.du.ac.bd>
2023-03-01 15:17:05 +0600

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,75 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+ const [owner, from1, from2, from3] = await hre.ethers.getSigners();
+ const chai = await hre.ethers.getContractFactory('chai');
+ const contract = await chai.deploy(); // Insstance of contract
+
+ await contract.deployed();
+ console.log("Address of contract: ", contract.address);
+
+ const addresses = [owner.address, from1.address, from2.address, from3.address];
+ console.log("Before buying chai ");
+
+ await consoleBalances(addresses);
+
+ const amount = {value:hre.ethers.utils.parseEther("2")}
+ await contract.connect(from1).buyChai("from1", "Very nice chai", amount);
+ await contract.connect(from2).buyChai("from2", "Very beautiful chai", amount);
+ await contract.connect(from3).buyChai("from3", "Rong chai", amount);
+
+console.log("After buying chai ");
+await consoleBalances(addresses);
+const memos = await contract.getMemos();
+consoleMemos(memos);
+
+}
+
+
+async function getBalances(address)
+ {
+  const balanceDigit = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceDigit);
+
+ }
+
+ async function consoleBalances (addresses)
+ {
+  let counter =0;
+  for(const address of addresses)
+  {
+    console.log(`Address ${counter} balance: `, await getBalances(address))
+  counter++;
+  
+  }
+ }
+
+ async function consoleMemos (memos) {
+
+ 
+  for(const memo of memos) {
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+
+    console.log(`At ${timestamp} ,name ${name} , address ${from}, message ${message}`);
+  }
+}
+
+
+
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
