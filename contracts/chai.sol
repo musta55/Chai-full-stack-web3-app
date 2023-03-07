a2398bede04921c6587217eb881ed1e253e6f67e
musta55 <bsse1114@iit.du.ac.bd>
2023-03-07 11:05:43 +0600

    Deployment tested

--- contracts/chai.sol
+++ contracts/chai.sol
@@ -11,18 +11,21 @@ contract chai {
 
     Memo[] memos;
     address payable owner;
-
-    constructor() {
+     
+     constructor () {
         owner = payable(msg.sender);
-    }
+     }
 
-    function buyChai(string memory name, string memory message) public payable {
-        require(msg.value > 0, "Please pay greater than 0 ether");
-        owner.transfer(msg.value);
+     function buyChai(string memory name,string memory message) public payable {
+        require( msg.value > 0,"Please pay greater then 0 ether");
+        owner.transfer (msg.value);
         memos.push(Memo(name, message, block.timestamp, msg.sender));
-    }
+     }
 
-    function getMemos() public view returns (Memo[] memory) {
+     function getMemos ()public view returns (Memo[] memory)
+     {
         return memos;
-    }
+     }
+
+
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,30 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
- const [owner, from1, from2, from3] = await hre.ethers.getSigners();
- const chai = await hre.ethers.getContractFactory('chai');
- const contract = await chai.deploy(); // Insstance of contract
+  const [owner,from1,from2,from3] = await hre.ethers.getSigners();
+  const chai = await hre.ethers.getContractFactory('chai');
+  const contract = await chai.deploy();     // Instance of contract
 
- await contract.deployed();
- console.log("Address of contract: ", contract.address);
+  await contract.deployed();
+  console.log("Address of the contract: ", contract.address);
 
- const addresses = [owner.address, from1.address, from2.address, from3.address];
- console.log("Before buying chai ");
+  const addresses = [owner.address, from1.address, from2.address, from3.address];
+  console.log("Before buying chai: ");
 
- await consoleBalances(addresses);
+  await consoleBalances(addresses);
+
 
  const amount = {value:hre.ethers.utils.parseEther("2")}
  await contract.connect(from1).buyChai("from1", "Very nice chai", amount);
  await contract.connect(from2).buyChai("from2", "Very beautiful chai", amount);
  await contract.connect(from3).buyChai("from3", "Rong chai", amount);
 
-console.log("After buying chai ");
-await consoleBalances(addresses);
-const memos = await contract.getMemos();
-consoleMemos(memos);
+  console.log("after buying chai: ");
+  await consoleBalances(addresses);
+  const memos = await  contract.getMemos();
+  consoleMemos(memos);
+
 
 }
 
@@ -63,9 +60,7 @@ async function getBalances(address)
   }
 }
 
-
-
-
+ 
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
